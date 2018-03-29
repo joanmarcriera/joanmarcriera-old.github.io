dfb6d998203ca32a075124c38679c5d653f18270
Joan Marc Riera Duocastella <joanmarcriera@gmail.com>
2018-03-29 21:56:46 +0100

    try plugin embed

--- _plugins/video-embeds.rb
+++ _plugins/video-embeds.rb
@@ -6,17 +6,17 @@ class YouTube < Liquid::Tag
     @id = args[0]
     @width = 500
     @height = 281
-    
+
     if args[1]
       @width = args[1]
       @height = args[2]
     end
   end
- 
+
   def render(context)
     "<div class=\"youtube\"><iframe width=\"#{@width}\" height=\"#{@height}\" src=\"http://www.youtube.com/embed/#{@id}?color=white&theme=light\"> </iframe></div>"
   end
- 
+
   Liquid::Template.register_tag("youtube", self)
 end
 
@@ -28,16 +28,16 @@ class Vimeo < Liquid::Tag
     @id = args[0]
     @width = 500
     @height = 281
-    
+
     if args[1]
       @width = args[1]
       @height = args[2]
     end
   end
- 
+
   def render(context)
     "<div class=\"vimeo\"><iframe src='http://player.vimeo.com/video/#{@id}\" width=\"#{@width}\" height=\"#{@height}\" frameborder=\"0\" webkitAllowFullScreen mozallowfullscreen allowFullScreen> </iframe></div>"
   end
- 
+
   Liquid::Template.register_tag("vimeo", self)
 end
